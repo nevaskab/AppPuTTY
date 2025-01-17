d0ef73da2bf33eb7f67006e9b2140a35b28b4f6e
Jobson Almeida <jobson.ra@outlook.com>
2025-01-17 14:51:58 -0300

    feat: criar função que liga um do LED de acordo a tecla pressionada e sua correpondência

--- AppPuTTY.c
+++ AppPuTTY.c
@@ -6,3 +6,14 @@
 #define LED_BLUE 12
 #define LED_RED 13
 #define BUZZER 21
+
+
+/// @brief Função que liga um led
+/// @param key Valor da tecla que correposnde ao seu respectivo LED
+void turn_on_led(uint key)
+{   
+    // valores das keys devem ser trocas nesse bloco conforme a necessidade
+    if (key == 10) gpio_put(LED_GREEN, 1);
+    if (key == 11) gpio_put(LED_BLUE, 1);
+    if (key == 12) gpio_put(LED_RED, 1);
+}
