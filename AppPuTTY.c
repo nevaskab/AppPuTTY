699a8a125c992c9de22efddf03b046fb76d02391
ErickRezende1833 <erick.rez.contato@gmail.com>
2025-01-17 14:42:49 -0300

    Atualizar o AppPuTTY.c

--- AppPuTTY.c
+++ AppPuTTY.c
@@ -6,3 +6,15 @@
 #define LED_BLUE 12
 #define LED_RED 13
 #define BUZZER 21
+
+void init_gpio() {
+    gpio_init(LED_GREEN);
+    gpio_set_dir(LED_GREEN, GPIO_OUT);
+    gpio_init(LED_BLUE);
+    gpio_set_dir(LED_BLUE, GPIO_OUT);
+    gpio_init(LED_RED);
+    gpio_set_dir(LED_RED, GPIO_OUT);
+    
+    gpio_init(BUZZER);
+    gpio_set_dir(BUZZER, GPIO_OUT);
+}
